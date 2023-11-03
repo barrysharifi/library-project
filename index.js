89a1431a4262621dc48825c702bac1fde4fdb4bc
Barrysharifi <barrysharifi@gmail.com>
2023-11-03 14:15:54 -0400

    function of the library completed with not very nice css, but it works!

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ function book (title, author, pages, read){
     this.author = author;
     this.pages= pages;
     this.read = read;
-    this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read'}`
-    }
+}
+//Read or not button
+book.prototype.toggleRead = function(){
+    this.read = !this.read
+}
+function toggleRead(index){
+    myLibrary[index].toggleRead()
+    render()
 }
 //created a library for books to be stored
 const myLibrary = []
@@ -14,23 +19,26 @@ function addBookToLibrary(){
    let title = document.querySelector("#title").value
    let author = document.getElementById("author").value
    let pages = document.getElementById("pages").value
-   let read = document.getElementById("read").value
+   let read = document.getElementById("read").checked
    let newBook = new book(title, author, pages, read)
    myLibrary.push(newBook)
    render()
 
 }
+
 //when button new book is clicked the form appears
 let newBookBtn = document.querySelector("#new-book-btn");
 newBookBtn.addEventListener("click", function(){
     let newBookForm = document.querySelector("#new-book-form");
     newBookForm.style.display = "block"
 })
-//submits new book into the console
+
+//submits new book into the library 
 document.querySelector("#new-book-form").addEventListener("submit", function(event){
     event.preventDefault()
     addBookToLibrary();
 })
+
 //loop through myLibrary adds a <p> element to #library
 function render(){
     let bookCase = document.querySelector("#library")
@@ -39,19 +47,29 @@ function render(){
     let book = myLibrary[i];
     let bookElement = document.createElement("div");
     bookElement.innerHTML = 
-    `<h3>${book.title}</h3> by
-    <h5>${book.author}</h5> 
-    <p>${book.pages}</p> pages 
-    <p>${book.read ? "Read" : "Not Read Yet"}</p>`
+    `<div class="card-header">
+        <h2 class="title">${book.title}</h2>
+        <h5 class="author">by ${book.author}</h5> 
+    </div>
+    <div class="card-body">
+        <p>${book.pages}</p> pages 
+        <p class ="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+    </div>
+    <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+    <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>`
     bookCase.appendChild(bookElement)
 }
 }
 
+//remove button
+function removeBook(index){
+    myLibrary.splice(index, 1)
+    render()
+}
 
 
 
 
 
-// const theHobbit = new book("The Hobbit", "J.R.R. Tolkien", 295, true)
 
 
